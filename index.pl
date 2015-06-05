055b765615c0710260077c777314ca742944fad4
James Nicholls <thejamesnicholls@gmail.com>
2015-06-06 00:32:08 +0100

    First attempt at formatting series data, probably not flexible enough

--- index.pl
+++ index.pl
@@ -25,13 +25,13 @@ sub splits {
 sub standings {
     my $split_id = shift;
     my $splits = splits;
-    my $standings;
+    my $standings = [];
     
     # Filter by split if provided and valid
-    my ($extra_clause, @params) = ('');
+    my ($split_filter, @params) = ('');
     $split_id = 0 unless (defined $split_id and (grep {$_->{id} == $split_id} @$splits));
     if ($split_id != 0) {
-        $extra_clause = 'AND split = ?';
+        $split_filter = 'AND split = ?';
         push @params, $split_id;
     }
 
@@ -49,7 +49,7 @@ sub standings {
         WHERE s.id in (summoner1, summoner2)
             AND r1.summoner = m.summoner1
             AND r2.summoner = m.summoner2
-            $extra_clause
+            $split_filter
         GROUP BY s.id
     }, { Slice => {} }, @params);
 
@@ -63,6 +63,109 @@ sub standings {
     }
 
     return $standings;
+}
+
+sub data_series {
+    my ($function, $split_id) = @_;
+    my $splits = splits;
+
+    # Data configuration
+    my $metric = 'pf';
+    my $aggregation = 'cumulative';
+    my $normalise = 0;
+    
+    # Filter by split if provided and valid
+    my ($split_filter, @params) = ('1');
+    $split_id = 0 unless (defined $split_id and (grep {$_->{id} == $split_id} @$splits));
+    if ($split_id != 0) {
+        $split_filter = 'split = ?';
+        push @params, $split_id;
+    }
+
+    my $summoners = $dbh->selectcol_arrayref(qq{
+        SELECT DISTINCT name
+        FROM summoner s
+        JOIN matchup m ON s.id IN (summoner1, summoner2)
+        JOIN result r USING (split, week)
+        WHERE $split_filter
+        ORDER BY s.id
+    }, {}, @params);
+    my $weeks = $dbh->selectcol_arrayref(qq{
+        SELECT DISTINCT CONCAT("S", split, " W", week)
+        FROM result
+        WHERE $split_filter
+        GROUP BY split, week
+        ORDER BY split, week
+    }, {}, @params);
+
+    my $data = $dbh->selectall_hashref(qq{
+        SELECT s.id, s.name, split, week,
+            IF ((r1.score > r2.score AND summoner1 = s.id) OR (r2.score > r1.score AND summoner2 = s.id), 1, 0) won,
+            IF  (r1.score = r2.score, 1, 0) tied,
+            IF ((r1.score > r2.score AND summoner2 = s.id) OR (r2.score > r1.score AND summoner1 = s.id), 1, 0) lost,
+            IF (summoner1 = s.id, r1.score, r2.score) pf,
+            IF (summoner1 = s.id, r2.score, r1.score) pa
+        FROM summoner s
+            JOIN matchup m ON s.id IN (summoner1, summoner2)
+            JOIN result r1 USING (split, week)
+            JOIN result r2 USING (split, week)
+        WHERE $split_filter
+            AND r1.summoner = m.summoner1
+            AND r2.summoner = m.summoner2
+        ORDER BY id, split, week
+    }, [qw/id split week/], {}, @params);
+    
+    my @results = ();
+
+    # Collect stats for summoners in each week
+    for (sort keys %$data) {
+        my $summoner = $data->{$_};
+        my @result = ();
+        my $total  = 0;
+
+        for (sort keys %$summoner) {
+            my $split = $summoner->{$_};
+            for (sort keys %$split) {
+                my $week = $split->{$_};
+
+                $total = $aggregation eq 'cumulative'
+                    ? $total + $week->{$metric} : $week->{$metric};
+                push @result, $total;
+            }
+        }
+        
+        # Pad the result to the correct number of weeks
+        #   This is necessary because some summoners joined late
+        #   If summoners start leaving, it will be worthwhile to
+        #   maintain a (split, week) -> week # mapping.
+        unshift @result, (0) x (scalar @$weeks - scalar @result);
+        push @results, \@result;
+    }
+
+#    return {
+#        labels => ["January", "February", "March", "April", "May", "June", "July"],
+#        series => ['Series A', 'Series B'],
+#        data => [
+#          [65, 59, 80, 81, 56, 55, 40],
+#          [28, 48, 40, 19, 86, 27, 90]
+#        ],
+#    };
+
+    return {
+        labels => $weeks,
+        series => $summoners,
+        data   => \@results,
+        #data   => [
+        #    [3,4,6,3,2,3,4,5,3,6,4,3,2,1,3,4,5,3,2,6,3],
+        #    [3,4,6,3,2,4,2,2,3,1,4,4,5,3,3,4,6,2,5,2,4],
+        #    [3,4,6,3,2,3,4,5,3,6,4,3,2,1,3,4,5,3,2,6,3],
+        #    [3,4,6,3,2,4,2,2,3,1,4,4,5,3,3,4,6,2,5,2,4],
+        #    [3,4,6,3,2,3,4,5,3,6,4,3,2,1,3,4,5,3,2,6,3],
+        #    [3,4,6,3,2,4,2,2,3,1,4,4,5,3,3,4,6,2,5,2,4],
+        #    [3,4,6,3,2,3,4,5,3,6,4,3,2,1,3,4,5,3,2,6,3],
+        #    [3,4,6,3,2,4,2,2,3,1,4,4,5,3,3,4,6,2,5,2,4],
+        #],
+    };
 }
 
 # Set up routes
@@ -80,6 +183,9 @@ get '/charts/pd/:split'     => sub { my $c = shift; $c->stash(page => 'charts/pd
 
 get '/api/standings'        => sub { my $c = shift; $c->render(json => standings()) };
 get '/api/standings/:split' => sub { my $c = shift; $c->render(json => standings($c->param('split'))) };
+
+get '/api/pf'               => sub { my $c = shift; $c->render(json => data_series('pf')) };
+get '/api/pf/:split'        => sub { my $c = shift; $c->render(json => data_series('pf', $c->param('split'))) };
 
 get '/'                     => sub { my $c = shift; $c->redirect_to('standings') };
 
--- public/js/salt.js
+++ public/js/salt.js
@@ -11,60 +11,21 @@ app.controller('StandingsCtrl', function($scope, $attrs, $http) {
     );
 });
 
-app.controller('ChartCtrl', function($scope, $attrs) {
-    $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
-    $scope.series = ['Series A', 'Series B'];
-    $scope.data = [
-        [65, 59, 80, 81, 56, 55, 40],
-        [28, 48, 40, 19, 86, 27, 90]
-    ];
+app.controller('ChartCtrl', function($scope, $attrs, $http) {
+    var api_url = ($attrs.split) ? '/api/pf/' + $attrs.split : '/api/pf';
+    $http.get(api_url)
+        .success(function(response) {
+console.log(response);
+            $scope.labels = response.labels;
+            $scope.series = response.series;
+            $scope.data   = response.data;
+        }
+    );
+//    $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
+//    $scope.series = ['Series A', 'Series B'];
+//    $scope.data = [
+//      [65, 59, 80, 81, 56, 55, 40],
+//      [28, 48, 40, 19, 86, 27, 90]
+//    ];
 });
-
-//app.controller('treeController', function($scope, $filter, $http) {
-//    var ipv4Filter = function(input) {
-//        return input.slice(-4).join('.');
-//    };
-//    var userKeys = [
-//        { name: 'name',         title: 'User name'      },
-//        { name: 'release',      title: 'Client version' },
-//        { name: 'os',           title: 'Operating system' },
-//        { name: 'osversion',    title: 'OS version'     },
-//        { name: 'onlinesecs',   title: 'Online time',   filter: 'interval' },
-//        { name: 'idlesecs',     title: 'Last activity', filter: 'ago' },
-////        { name: 'address',      title: 'IP address',    filter: 'ipv4' },
-//    ];
-//    var channelKeys = [
-//        { name: 'name',         title: 'Channel name'   },
-//        { name: 'id',           title: 'Channel ID'     },
-//        { name: 'description',  title: 'Description'    },
-//    ];
-//
-//    $scope.summary = 'Select a user or channel for details';
-//    $scope.onTreeReady = function(e, data) {
-//        $('#treeView').on('changed.jstree', function (e, data) {
-//            if (data.action == 'select_node') {
-//                var node = data.node.original;
-//                $http.get('/api/node.php?type=' + node.type + '&id=' + node.id)
-//                    .success(function(response) {
-//                        $scope.summary  = '';
-//                        $scope.info     = [];
-//
-//                        // Select data keys depending on node type
-//                        var keys = (node.type == 'user') ? userKeys : channelKeys;
-//                        angular.forEach(keys, function(item) {
-//                            var val = response[item.name];
-//
-//                            this.push({
-//                                title: item.title,
-//                                value: $filter(item.filter || 'identity')(val),
-//                            });
-//                        }, $scope.info);
-//                    }
-//                );
-//            } else {
-//                console.log("Unhandled action " + data.action);
-//            }
-//        });
-//    };
-//});
 
